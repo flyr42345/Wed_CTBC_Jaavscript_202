b0e761e206b97306832f23e261a19af705e72de7
flyr42345 <linj56290@gmail.com>
2024-12-12 12:09:12 +0800

    Upte建立基礎設定

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("這是外部的JS");
+
+// 變數variable(會變的數值)
+// 儲存網站內的資料，例如:顏色、字體大小、元素、使用者資訊
+
+// 變數語法
+// let變數名稱;
+
+// 變數名稱命名習慣(守則)
+// 1.不會使用中文命名
+// 2.不使用數字開頭
+// 3.不使用全形符號@
+// 4.空格 test 1
+// 5.保留字(有作用的關鍵字) if、let
+
+
+let test = 7;
+let fontColor = "#f63";
+
+// 取得變數get
+console.log(test);
+console.log(fontColor);
+
+// 設定變數set
+test = 9;
+console.log(test);
